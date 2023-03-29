7f8d6cc7e32eedfa7203a5d0ab96f70145f851e7
Tim Child <timjchild@phas.ubc.ca>
2023-03-29 13:26:40 -0700

    deprecate dash_util
    Instead use dat_analysis.dash.util

--- dash_util.py
+++ dash_util.py
@@ -1,4 +1,6 @@
 """
 2023-03-28 -- All useful functions moved to dat_analysis (and imported here for backwards-compatability)
 """
+import warnings
+warnings.warn(f'dash_util is deprecated. You should change any imports from `dash_util` to be from `dat_analysis.dash.util` instead!')
 from dat_analysis.dash.util import *
